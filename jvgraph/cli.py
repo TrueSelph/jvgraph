93fa5b4595aae2c279d2fa1906e6800161a976d4
Gimel Dick <gimeldick@gmail.com>
2025-07-10 16:01:10 -0400

    first commit

---
+++ jvgraph/cli.py
@@ -0,0 +1,13 @@
+"""Jivas Graph CLI tool."""
+
+from jvgraph import __version__
+from jvgraph.group import jvgraph
+from jvgraph.commands.launch import launch
+
+
+# Register command groups
+jvgraph.add_command(launch)
+
+
+if __name__ == "__main__":
+    jvgraph()  # pragma: no cover
